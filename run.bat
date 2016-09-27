7aa18ca87ff273b8f45e090dcda64811ee54bbb6
Vijay <vijayitjob@gmail.com>
2016-09-27 12:40:48 +0530

    update

---
+++ run.bat
@@ -0,0 +1,2 @@
+set path=%path%;.\sikuli-ide\libs
+java -cp bin;.\ext\selenium-server-standalone-2.48.2.jar;.\ext\jtidy-r872-jdk15.jar;.\ext\jxl.jar;.\ext\log4j-1.2.16.jar;.\ext\mail.jar;.\ext\java-client-1.7.0.jar;.\ext\gson-2.2.4.jar;.\ext\iSafe2.0.jar;.\ext\activation.jar;.\ext\itextpdf-5.2.1.jar iSAFE.TestDriver
